538e670e96d2c260a6ea0154cf9a27fbc3da3ddb
Paul Szefer <pszefer@hotmail.com>
2019-09-03 21:49:22 -0700

    Remove size-dependent methods from List
    A List is a collection of items. The internal storage of these items (the
    array and its size) should not be visible to the user.

--- src/io/github/paulszefer/List.java
+++ src/io/github/paulszefer/List.java
@@ -14,24 +14,9 @@ public class List {
     public static final int DEFAULT_STARTING_SIZE = 32;
 
     /**
-     * Creates an empty list with size {@value #DEFAULT_STARTING_SIZE}.
+     * Creates an empty list.
      */
     public List() {};
-
-    /**
-     * Creates an empty list with the given size.
-     *
-     * @param size the initial size of the list
-     */
-    public List(int size) {};
-
-    /**
-     * Creates an list with the given size, filled with the given element.
-     *
-     * @param size the initial size of the list
-     * @param value the value to fill each element with
-     */
-    public List(int size, int value) {};
 
     /**
      * Creates a list to hold the elements of the given list.
@@ -132,39 +117,6 @@ public class List {
      * @return the removed element
      */
     public int removeLast() { return 0; };
-
-    /**
-     * Fills the list with the given value.
-     *
-     * @param value the value to fill each element with
-     */
-    public void fill(int value) {};
-
-    /**
-     * Fills the list with the given value from {@code start} to the end of the list.
-     *
-     * @param value the value to fill each element with
-     * @param start the index to start filling at (inclusive)
-     */
-    public void fill(int value, int start) {};
-
-    /**
-     * Fills the list with the given value from {@code start} to {@code end}.
-     *
-     * @param value the value to fill each element with
-     * @param start the index to start filling at (inclusive)
-     * @param end the index to stop filling at (exclusive)
-     */
-    public void fill(int value, int start, int end) {};
-
-    /**
-     * Fills the list based on the given parameters.
-     *
-     * @param startValue the value to start at
-     * @param change the value to add to each previous value to get the next
-     * @param factor the value to multiply each previous value by to get the next
-     */
-    public void fillCustom(int startValue, int change, int factor) {};
 
     /**
      * Empties the list.
