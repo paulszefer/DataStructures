892de6fea6008a3d46850d12892edda4ee94e815
Paul Szefer <pszefer@hotmail.com>
2019-09-06 22:22:38 -0700

    Add more test methods

--- src/io/github/paulszefer/ListTest.java
+++ src/io/github/paulszefer/ListTest.java
@@ -1,14 +1,21 @@
 package io.github.paulszefer;
 
+import static io.github.paulszefer.TestAsserts.assertEqual;
+import static io.github.paulszefer.TestAsserts.assertThrows;
+
 public class ListTest extends Test {
 
     private List list1;
     private List list2;
+    private List list3;
 
     public void setUp() {
         list1 = new List();
         list2 = new List(new int[] {
            0, 1, 2, 3, 4, 5
+        });
+        list3 = new List(new int[] {
+           3, 7, -1, -2, -10
         });
     }
 
@@ -17,21 +24,71 @@ public class ListTest extends Test {
         assertEqual(0, list1.size());
     }
 
-    public void test_List_Array_GivenEmptyArray_ListIsEmpty() throws Exception {
+    public void test_List_EmptyArray_ListIsEmpty() throws Exception {
         int[] arr = new int[] { };
         list1 = new List(arr);
         assertEqual(0, list1.size());
     }
 
-    public void test_List_Array_GivenArray_HasCorrectSize() throws Exception {
+    public void test_List_Array_HasCorrectSize() throws Exception {
         int[] arr = new int[] { 0, 1 };
         list1 = new List(arr);
         assertEqual(2, list1.size());
     }
 
-    public void test_List_Array_GivenArray_HasCorrectSize2() throws Exception {
+    public void test_List_Array_HasCorrectSize2() throws Exception {
         int[] arr = new int[] { -2, -3, -4 };
         list1 = new List(arr);
         assertEqual(3, list1.size());
+    }
+
+    public void test_copy_EmptyList_CreatesEmptyList() throws Exception {
+        List list = list1.copy();
+        assertEqual(0, list.size());
+    }
+
+    public void test_copy_List_CreatesCorrectList() throws Exception {
+        List list = list2.copy();
+        assertEqual(6, list.size());
+        assertEqual(0, list.get(0));
+        assertEqual(1, list.get(1));
+        assertEqual(2, list.get(2));
+        assertEqual(3, list.get(3));
+        assertEqual(4, list.get(4));
+        assertEqual(5, list.get(5));
+    }
+
+    public void test_copy_List_CreatesCorrectList2() throws Exception {
+        List list = list3.copy();
+        assertEqual(  5, list.size());
+        assertEqual(  3, list.get(0));
+        assertEqual(  7, list.get(1));
+        assertEqual( -1, list.get(2));
+        assertEqual( -2, list.get(3));
+        assertEqual(-10, list.get(4));
+    }
+
+    public void test_first_EmptyList_ThrowsException() throws Exception {
+        assertThrows(ListEmptyException.class, list1::first);
+    }
+
+    public void test_first_List_GetsCorrectValue() throws Exception {
+        assertEqual(0, list2.first());
+    }
+
+    public void test_first_List_GetsCorrectValue2() throws Exception {
+        assertEqual(3, list3.first());
+    }
+
+    public void test_last_EmptyList_ThrowsException() throws Exception {
+        assertThrows(ListEmptyException.class, list1::last);
+    }
+
+    public void test_last_List_GetsCorrectValue() throws Exception {
+        assertEqual(5, list2.last());
+    }
+
+    public void test_last_List_GetsCorrectValue2() throws Exception {
+        assertEqual(-10, list3.last());
     }
 }
