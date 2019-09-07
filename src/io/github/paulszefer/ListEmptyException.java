9b821fc9c00a03c1789844ba0e7981e05e129eb2
Paul Szefer <pszefer@hotmail.com>
2019-09-06 19:33:37 -0700

    Add ListEmptyException

---
+++ src/io/github/paulszefer/ListEmptyException.java
@@ -0,0 +1,7 @@
+package io.github.paulszefer;
+
+/**
+ * An exception that is thrown when the user tries to retrieve or remove from an
+ * empty list.
+ */
+public class ListEmptyException extends RuntimeException {}
