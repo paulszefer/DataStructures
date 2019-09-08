65540ed3626e89c99784d9bcbebaab5c8987c154
Paul Szefer <pszefer@hotmail.com>
2019-09-08 03:38:04 -0700

    Add remaining ListTest tests

--- src/io/github/paulszefer/ListTest.java
+++ src/io/github/paulszefer/ListTest.java
@@ -113,10 +113,10 @@ public class ListTest extends Test {
     }
 
     public void test_get_List_IndexTooHigh_ThrowsException() throws Exception {
-        assertThrows(IndexOutOfBoundsException.class, () -> list3.get(5));
+        assertThrows(IndexOutOfBoundsException.class, () -> list3.get(LIST3_SIZE));
     }
 
-    public void test_get_List_StartIndex_GetsCorrectValue() throws Exception {
+    public void test_get_List_FirstIndex_GetsCorrectValue() throws Exception {
         assertEqual(3, list3.get(0));
     }
 
@@ -124,12 +124,11 @@ public class ListTest extends Test {
         assertEqual(7, list3.get(1));
     }
 
-    public void test_get_List_EndIndex_GetsCorrectValue() throws Exception {
-        assertEqual(-10, list3.get(4));
+    public void test_get_List_LastIndex_GetsCorrectValue() throws Exception {
+        assertEqual(-10, list3.get(LIST3_SIZE - 1));
     }
 
     public void test_add_EmptyList() throws Exception {
-        assertEqual(LIST1_SIZE, list1.size());
         int value = 5;
         list1.add(value);
         assertEqual(LIST1_SIZE + 1, list1.size());
@@ -137,7 +136,6 @@ public class ListTest extends Test {
     }
 
     public void test_add_List() throws Exception {
-        assertEqual(LIST2_SIZE, list2.size());
         int value = 10;
         list2.add(value);
         assertEqual(LIST2_SIZE + 1, list2.size());
@@ -145,7 +143,6 @@ public class ListTest extends Test {
     }
 
     public void test_add_List_ExtendsListSize() throws Exception {
-        assertEqual(LIST4_SIZE, list4.size());
         int value = 15;
         list4.add(value);
         assertEqual(LIST4_SIZE + 1, list4.size());
@@ -163,36 +160,215 @@ public class ListTest extends Test {
     }
 
     public void test_insert_List_IndexTooHigh_ThrowsException() throws Exception {
-        assertThrows(IndexOutOfBoundsException.class, () -> list2.insert(10, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> list2.insert(10, LIST2_SIZE));
     }
 
-    public void test_insert_List_StartIndex() throws Exception {
-        assertEqual(LIST3_SIZE, list3.size());
+    public void test_insert_List_FirstIndex() throws Exception {
         int value = 10;
         list3.insert(value, 0);
         assertEqual(LIST3_SIZE + 1, list3.size());
         assertEqual(value, list3.get(0));
         assertEqual(3, list3.get(1));
-        assertEqual(-10, list3.get(6));
+        assertEqual(-10, list3.get(LIST3_SIZE));
     }
 
     public void test_insert_List_MiddleIndex() throws Exception {
-        assertEqual(LIST3_SIZE, list3.size());
         int value = 10;
         list3.insert(value, 1);
         assertEqual(LIST3_SIZE + 1, list3.size());
         assertEqual(3, list3.get(0));
         assertEqual(value, list3.get(1));
-        assertEqual(-10, list3.get(6));
+        assertEqual(-10, list3.get(LIST3_SIZE));
     }
 
-    public void test_insert_List_EndIndex() throws Exception {
-        assertEqual(LIST3_SIZE, list3.size());
+    public void test_insert_List_LastIndex() throws Exception {
         int value = 10;
-        list3.insert(value, 5);
+        list3.insert(value, LIST3_SIZE - 1);
         assertEqual(LIST3_SIZE + 1, list3.size());
         assertEqual(3, list3.get(0));
-        assertEqual(value, list3.get(5));
-        assertEqual(-10, list3.get(6));
+        assertEqual(value, list3.get(LIST3_SIZE - 1));
+        assertEqual(-10, list3.get(LIST3_SIZE));
+    }
+
+    public void test_remove_List_NegativeIndex_ThrowsException() throws Exception {
+        assertThrows(IndexOutOfBoundsException.class, () -> list2.remove(-1));
+    }
+
+    public void test_remove_List_IndexTooHigh_ThrowsException() throws Exception {
+        assertThrows(IndexOutOfBoundsException.class, () -> list2.remove(LIST2_SIZE));
+    }
+
+    public void test_remove_List_FirstIndex() throws Exception {
+        int value = list3.remove(0);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(3, value);
+        assertEqual(7, list3.get(0));
+        assertEqual(-1, list3.get(1));
+        assertEqual(-10, list3.get(LIST3_SIZE - 2));
+    }
+
+    public void test_remove_List_MiddleIndex() throws Exception {
+        int value = list3.remove(1);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(7, value);
+        assertEqual(3, list3.get(0));
+        assertEqual(-1, list3.get(1));
+        assertEqual(-10, list3.get(LIST3_SIZE - 2));
+    }
+
+    public void test_remove_List_LastIndex() throws Exception {
+        int value = list3.remove(LIST3_SIZE - 1);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(-10, value);
+        assertEqual(3, list3.get(0));
+        assertEqual(-1, list3.get(LIST3_SIZE - 3));
+        assertEqual(-2, list3.get(LIST3_SIZE - 2));
+    }
+
+    public void test_remove_start_end_EmptyList_ThrowsException() throws Exception {
+        assertThrows(ListEmptyException.class, () -> list1.remove(0, 1));
+    }
+
+    public void test_remove_start_end_List_StartIndexNegative_ThrowsException() throws Exception {
+        assertThrows(IndexOutOfBoundsException.class, () -> list2.remove(-1, 1));
+    }
+
+    public void test_remove_start_end_List_EndIndexNegative_ThrowsException() throws Exception {
+        assertThrows(IndexOutOfBoundsException.class, () -> list2.remove(-1, -1));
+        // list2.remove(0, -1) caught by test_remove_start_end_List_StartIndexLargerThanEndIndex_ThrowsException
+        // list2.remove(-2, -1) caught by test_remove_start_end_List_StartIndexNegative_ThrowsException
+    }
+
+    public void test_remove_start_end_List_StartIndexEqualToEndIndex_ReturnsEmptyList() throws Exception {
+        int start = 0;
+        int end = 0;
+        List list = list3.remove(start, end);
+        assertEqual(0, list.size());
+    }
+
+    public void test_remove_start_end_List_StartIndexLargerThanEndIndex_ThrowsException() throws Exception {
+        assertThrows(IllegalArgumentException.class, () -> list2.remove(1, 0));
+    }
+
+    public void test_remove_start_end_List_RemoveOneFromStart() throws Exception {
+        int start = 0;
+        int end = 1;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(3, list.get(0));
+        assertEqual(7, list3.get(0));
+        assertEqual(-1, list3.get(1));
+        assertEqual(-2, list3.get(2));
+        assertEqual(-10, list3.get(3));
+    }
+
+    public void test_remove_start_end_List_RemoveOneFromMiddle() throws Exception {
+        int start = 1;
+        int end = 2;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(7, list.get(0));
+        assertEqual(3, list3.get(0));
+        assertEqual(-1, list3.get(1));
+        assertEqual(-2, list3.get(2));
+        assertEqual(-10, list3.get(3));
+    }
+
+    public void test_remove_start_end_List_RemoveOneFromEnd() throws Exception {
+        int start = LIST3_SIZE - 1;
+        int end = LIST3_SIZE;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(-10, list.get(0));
+        assertEqual(3, list3.get(0));
+        assertEqual(7, list3.get(1));
+        assertEqual(-1, list3.get(2));
+        assertEqual(-2, list3.get(3));
+    }
+
+    public void test_remove_start_end_List_RemoveMultipleFromStart() throws Exception {
+        int start = 0;
+        int end = 3;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - end + start, list3.size());
+        assertEqual(3, list.get(0));
+        assertEqual(7, list.get(1));
+        assertEqual(-1, list.get(2));
+        assertEqual(-2, list3.get(0));
+        assertEqual(-10, list3.get(1));
+    }
+
+    public void test_remove_start_end_List_RemoveMultipleFromMiddle() throws Exception {
+        int start = 1;
+        int end = 3;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - end + start, list3.size());
+        assertEqual(7, list.get(0));
+        assertEqual(-1, list.get(1));
+        assertEqual(3, list3.get(0));
+        assertEqual(-2, list3.get(1));
+        assertEqual(-10, list3.get(2));
+    }
+
+    public void test_remove_start_end_List_RemoveMultipleFromEnd() throws Exception {
+        int start = 2;
+        int end = LIST3_SIZE;
+        List list = list3.remove(start, end);
+        assertEqual(LIST3_SIZE - end + start, list3.size());
+        assertEqual(-1, list.get(0));
+        assertEqual(-2, list.get(1));
+        assertEqual(-10, list.get(2));
+        assertEqual(3, list3.get(0));
+        assertEqual(7, list3.get(1));
+    }
+
+    public void test_remove_start_end_List_RemoveAll() throws Exception {
+        int start = 0;
+        int end = LIST3_SIZE;
+        List list = list3.remove(start, end);
+        assertEqual(0, list3.size());
+        assertEqual(3, list.get(0));
+        assertEqual(7, list.get(1));
+        assertEqual(-1, list.get(2));
+        assertEqual(-2, list.get(3));
+        assertEqual(-10, list.get(4));
+    }
+
+    public void test_removeFirst_EmptyList_ThrowsException() throws Exception {
+        assertThrows(ListEmptyException.class, () -> list1.removeFirst());
+    }
+
+    public void test_removeFirst_List() throws Exception {
+        int value = list3.removeFirst();
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(3, value);
+        assertEqual(7, list3.get(0));
+        assertEqual(-1, list3.get(1));
+        assertEqual(-2, list3.get(2));
+        assertEqual(-10, list3.get(3));
+    }
+
+    public void test_removeLast_EmptyList_ThrowsException() throws Exception {
+        assertThrows(ListEmptyException.class, () -> list1.removeLast());
+    }
+
+    public void test_removeLast_List() throws Exception {
+        int value = list3.removeLast();
+        assertEqual(LIST3_SIZE - 1, list3.size());
+        assertEqual(-10, value);
+        assertEqual(3, list3.get(0));
+        assertEqual(7, list3.get(1));
+        assertEqual(-1, list3.get(2));
+        assertEqual(-2, list3.get(3));
+    }
+
+    public void test_empty_EmptyList() throws Exception {
+        list1.empty();
+        assertEqual(0, list1.size());
+    }
+
+    public void test_empty_List() throws Exception {
+        list2.empty();
+        assertEqual(0, list2.size());
     }
 }
