07737075f2dfafe02333bcbf7dc65621a16282b1
Paul Szefer <pszefer@hotmail.com>
2019-09-08 22:16:50 -0700

    Modify build script to display more info on build warnings/errors

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 if not exist .\bin\io\github\paulszefer\ mkdir .\bin\io\github\paulszefer\
-javac .\src\io\github\paulszefer\*.java -d .\bin
+javac .\src\io\github\paulszefer\*.java -d .\bin -Xlint:unchecked
